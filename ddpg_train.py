27239a0eba236eee6bb3e387f8ca41d5b41e6ac6
vincent <602933854@qq.com>
2021-04-21 17:21:07 +0800

    修改模型参数量查看code

--- ddpg_train.py
+++ ddpg_train.py
@@ -9,144 +9,155 @@ import torch.nn as nn
 import torch.optim as optim
 import torch.nn.functional as F
 from torch.distributions import Normal
-
 import matplotlib.pyplot as plt
+from torchsummary import summary
 use_cuda = torch.cuda.is_available()
-device   = torch.device("cuda" if use_cuda else "cpu")
+device = torch.device("cuda" if use_cuda else "cpu")
+
 
 class ReplayBuffer:
     def __init__(self, capacity):
         self.capacity = capacity
         self.buffer = []
         self.position = 0
-    
+
     def push(self, state, action, reward, next_state, done):
         if len(self.buffer) < self.capacity:
             self.buffer.append(None)
         self.buffer[self.position] = (state, action, reward, next_state, done)
         self.position = (self.position + 1) % self.capacity
-    
+
     def sample(self, batch_size):
         batch = random.sample(self.buffer, batch_size)
         state, action, reward, next_state, done = map(np.stack, zip(*batch))
         return state, action, reward, next_state, done
-    
+
     def __len__(self):
         return len(self.buffer)
+
 
 class NormalizedActions(gym.ActionWrapper):
 
     def _action(self, action):
-        low_bound   = self.action_space.low
+        low_bound = self.action_space.low
         upper_bound = self.action_space.high
-        
+
         action = low_bound + (action + 1.0) * 0.5 * (upper_bound - low_bound)
         action = np.clip(action, low_bound, upper_bound)
-        
+
         return action
 
     def _reverse_action(self, action):
-        low_bound   = self.action_space.low
+        low_bound = self.action_space.low
         upper_bound = self.action_space.high
-        
+
         action = 2 * (action - low_bound) / (upper_bound - low_bound) - 1
         action = np.clip(action, low_bound, upper_bound)
-        
+
         return action
+
 
 class OUNoise(object):
     def __init__(self, action_space, mu=0.0, theta=0.15, max_sigma=0.3, min_sigma=0.3, decay_period=100000):
-        self.mu           = mu
-        self.theta        = theta
-        self.sigma        = max_sigma
-        self.max_sigma    = max_sigma
-        self.min_sigma    = min_sigma
+        self.mu = mu
+        self.theta = theta
+        self.sigma = max_sigma
+        self.max_sigma = max_sigma
+        self.min_sigma = min_sigma
         self.decay_period = decay_period
-        self.action_dim   = action_space.shape[0]
-        self.low          = action_space.low
-        self.high         = action_space.high
+        self.action_dim = action_space.shape[0]
+        self.low = action_space.low
+        self.high = action_space.high
         self.reset()
-        
+
     def reset(self):
         self.state = np.ones(self.action_dim) * self.mu
-        
+
     def evolve_state(self):
-        x  = self.state
-        dx = self.theta * (self.mu - x) + self.sigma * np.random.randn(self.action_dim)
+        x = self.state
+        dx = self.theta * (self.mu - x) + self.sigma * \
+            np.random.randn(self.action_dim)
         self.state = x + dx
         return self.state
-    
+
     def get_action(self, action, t=0):
         ou_state = self.evolve_state()
-        self.sigma = self.max_sigma - (self.max_sigma - self.min_sigma) * min(1.0, t / self.decay_period)
+        self.sigma = self.max_sigma - \
+            (self.max_sigma - self.min_sigma) * min(1.0, t / self.decay_period)
         return np.clip(action + ou_state, self.low, self.high)
+
 
 class ValueNetwork(nn.Module):
     def __init__(self, num_inputs, num_actions, hidden_size, init_w=3e-3):
         super(ValueNetwork, self).__init__()
-        
+
         self.linear1 = nn.Linear(num_inputs + num_actions, hidden_size)
         self.linear2 = nn.Linear(hidden_size, hidden_size)
         self.linear3 = nn.Linear(hidden_size, 1)
-        
+
         self.linear3.weight.data.uniform_(-init_w, init_w)
         self.linear3.bias.data.uniform_(-init_w, init_w)
-        
+
     def forward(self, state, action):
-        x = torch.cat([state, action], 1)
+        print('forward:')
+        print(state.shape)
+        print(action.shape)
+        x = torch.cat([state, action], -1)
         x = F.relu(self.linear1(x))
         x = F.relu(self.linear2(x))
         x = self.linear3(x)
         return x
-    
+
 
 class PolicyNetwork(nn.Module):
     def __init__(self, num_inputs, num_actions, hidden_size, init_w=3e-3):
         super(PolicyNetwork, self).__init__()
-        
+
         self.linear1 = nn.Linear(num_inputs, hidden_size)
         self.linear2 = nn.Linear(hidden_size, hidden_size)
         self.linear3 = nn.Linear(hidden_size, num_actions)
-        
+
         self.linear3.weight.data.uniform_(-init_w, init_w)
         self.linear3.bias.data.uniform_(-init_w, init_w)
-        
+
     def forward(self, state):
         x = F.relu(self.linear1(state))
         x = F.relu(self.linear2(x))
         x = F.tanh(self.linear3(x))
         return x
-    
+
     def get_action(self, state):
-        state  = torch.FloatTensor(state).unsqueeze(0).to(device)
+        state = torch.FloatTensor(state).unsqueeze(0).to(device)
         action = self.forward(state)
         return action.detach().cpu().numpy()[0, 0]
-# test for  
-def ddpg_update(batch_size=128,
-           gamma = 0.99,
-           min_value=-np.inf,
-           max_value=np.inf,
-           soft_tau=1e-2):
-    
+
+# test for  batch_size=128, 上下文会提供 batch_size 的内容
+
+
+def ddpg_update(
+        gamma=0.99,
+        min_value=-np.inf,
+        max_value=np.inf,
+        soft_tau=1e-2):
+
     state, action, reward, next_state, done = replay_buffer.sample(batch_size)
-    
-    state      = torch.FloatTensor(state).to(device)
+
+    state = torch.FloatTensor(state).to(device)
     next_state = torch.FloatTensor(next_state).to(device)
-    action     = torch.FloatTensor(action).to(device)
-    reward     = torch.FloatTensor(reward).unsqueeze(1).to(device)
-    done       = torch.FloatTensor(np.float32(done)).unsqueeze(1).to(device)
+    action = torch.FloatTensor(action).to(device)
+    reward = torch.FloatTensor(reward).unsqueeze(1).to(device)
+    done = torch.FloatTensor(np.float32(done)).unsqueeze(1).to(device)
 
     policy_loss = value_net(state, policy_net(state))
     policy_loss = -policy_loss.mean()
 
-    next_action    = target_policy_net(next_state)
-    target_value   = target_value_net(next_state, next_action.detach())
+    next_action = target_policy_net(next_state)
+    target_value = target_value_net(next_state, next_action.detach())
     expected_value = reward + (1.0 - done) * gamma * target_value
     expected_value = torch.clamp(expected_value, min_value, max_value)
 
     value = value_net(state, action)
     value_loss = value_criterion(value, expected_value.detach())
-
 
     policy_optimizer.zero_grad()
     policy_loss.backward()
@@ -157,26 +168,27 @@ def ddpg_update(batch_size=128,
     value_optimizer.step()
 
     for target_param, param in zip(target_value_net.parameters(), value_net.parameters()):
-            target_param.data.copy_(
-                target_param.data * (1.0 - soft_tau) + param.data * soft_tau
-            )
+        target_param.data.copy_(
+            target_param.data * (1.0 - soft_tau) + param.data * soft_tau
+        )
 
     for target_param, param in zip(target_policy_net.parameters(), policy_net.parameters()):
-            target_param.data.copy_(
-                target_param.data * (1.0 - soft_tau) + param.data * soft_tau
-            )
+        target_param.data.copy_(
+            target_param.data * (1.0 - soft_tau) + param.data * soft_tau
+        )
+
 
 env = NormalizedActions(gym.make("Pendulum-v0"))
 ou_noise = OUNoise(env.action_space)
 
-state_dim  = env.observation_space.shape[0]
+state_dim = env.observation_space.shape[0]
 action_dim = env.action_space.shape[0]
 hidden_dim = 256
 
-value_net  = ValueNetwork(state_dim, action_dim, hidden_dim).to(device)
+value_net = ValueNetwork(state_dim, action_dim, hidden_dim).to(device)
 policy_net = PolicyNetwork(state_dim, action_dim, hidden_dim).to(device)
 
-target_value_net  = ValueNetwork(state_dim, action_dim, hidden_dim).to(device)
+target_value_net = ValueNetwork(state_dim, action_dim, hidden_dim).to(device)
 target_policy_net = PolicyNetwork(state_dim, action_dim, hidden_dim).to(device)
 
 for target_param, param in zip(target_value_net.parameters(), value_net.parameters()):
@@ -184,29 +196,30 @@ for target_param, param in zip(target_value_net.parameters(), value_net.paramete
 
 for target_param, param in zip(target_policy_net.parameters(), policy_net.parameters()):
     target_param.data.copy_(param.data)
-    
-    
-value_lr  = 1e-3
+
+
+value_lr = 1e-3
 policy_lr = 1e-4
 
-value_optimizer  = optim.Adam(value_net.parameters(),  lr=value_lr)
+value_optimizer = optim.Adam(value_net.parameters(),  lr=value_lr)
 policy_optimizer = optim.Adam(policy_net.parameters(), lr=policy_lr)
 
 value_criterion = nn.MSELoss()
 
 replay_buffer_size = 1000000
 replay_buffer = ReplayBuffer(replay_buffer_size)
+batch_size = 128
 
+summary(value_net, input_size=(1, state_dim), action_size=(1, action_dim))
+summary(policy_net, input_size=(1, state_dim))
+summary(target_value_net, input_size=(
+    1, state_dim), action_size=(1, action_dim))
+summary(target_policy_net, input_size=(1, state_dim))
 
-max_frames  = 12000
-max_steps   = 500
-frame_idx   = 0
-rewards     = []
-batch_size  = 128
 
 def ddpg_policy_update():
     paramters_dict = policy_net.state_dict()
     paramters_dict = {
-        k:v.data.tolist() for k,v in paramters_dict.items()
+        k: v.data.tolist() for k, v in paramters_dict.items()
     }
     return paramters_dict
